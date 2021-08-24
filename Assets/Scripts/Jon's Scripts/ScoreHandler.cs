4c92ec2fa405f88964addbedb53edcb7dcb300fb
Jon <jongold00@gmail.com>
2021-08-24 14:12:08 -0400

    Combo Meter

--- Assets/Scripts/Jon's Scripts/ScoreHandler.cs
+++ Assets/Scripts/Jon's Scripts/ScoreHandler.cs
@@ -28,6 +28,27 @@ public class ScoreHandler : MonoBehaviour
 
     private bool slamming = false;
 
+    /// <summary>
+    /// Combo stuff
+    /// </summary>
+    public float comboTimer;
+
+    [SerializeField]
+    [Range(0, 10)]
+    int comboThreshhold;
+
+    [SerializeField]
+    TextMeshProUGUI multiplierTextField;
+
+    [SerializeField]
+    Image comboBarFill;
+
+    [SerializeField]
+    int[] comboArray;
+
+    private int currentComboIndex = 0;  // gets the current multiplier
+    private int currentComboChain = 0;  // how many items ive destroyed / progress towards next multiplier
+    private float currentComboTimer = 0;  // how long until current combo expires
     public void Start()
     {
         maxScorePossible = GetMaxScorePossible();
@@ -49,11 +70,21 @@ public class ScoreHandler : MonoBehaviour
             scoreBarGO.transform.localScale = Vector3.Lerp(scoreBarGO.transform.localScale, baselineBarScale, slamShrinkFactor);
         }
 
+        currentComboTimer -= Time.deltaTime;
+        comboBarFill.fillAmount = currentComboTimer / comboTimer;
+        if (currentComboTimer <= 0)
+        {
+            currentComboIndex = 0;
+            currentComboChain = 0;
+            multiplierTextField.text = "COMBO! " + comboArray[currentComboIndex].ToString() + "x";
+        }
+
     }
 
     public void UpdateScore(int delta)
     {
-        score += delta;
+        UpdateComboChain();
+        score += ApplyComboMultiplier(delta);
         ChangeScoreText();
         SetBarFill();
         BarSlamEffect();
@@ -83,5 +114,25 @@ public class ScoreHandler : MonoBehaviour
     {
         scoreBarGO.transform.localScale = new Vector3(baselineBarScale.x + 1, baselineBarScale.y + 1, 1);
         slamming = true;
+    }
+
+    /// <summary>
+    /// Stuff having to do with Combos
+    /// </summary>
+    void UpdateComboChain()
+    {
+        currentComboTimer = comboTimer;
+        currentComboChain++;
+        if (currentComboChain % comboThreshhold == 0)
+        {
+            currentComboIndex = Mathf.Min(comboArray.Length - 1, currentComboIndex + 1);
+            currentComboChain = 0;
+        }
+        multiplierTextField.text = "COMBO! " + comboArray[currentComboIndex].ToString() + "x";
+    }
+
+    float ApplyComboMultiplier(float delta)
+    {
+        return delta * comboArray[currentComboIndex];
     }
 }

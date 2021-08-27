750e1948e9b7b7d2b9002773c48540df5071ad4a
Alexandre Alves <pynjunior@gmail.com>
2021-08-28 00:20:36 +0100

    Destructible and particles

--- Assets/Scripts/Jon's Scripts/Destructible.cs
+++ Assets/Scripts/Jon's Scripts/Destructible.cs
@@ -6,29 +6,38 @@ public class Destructible : MonoBehaviour
 {
     public int destroyValue;
 
-    private float forceToDestroy;
-    public static float destroyCountdown = 2.5f;
+    public float forceToDestroy;
+    public static float destroyCountdown = 1.5f;
+    public GameObject particles;
+
+    private Rigidbody rb;
+    private Vector3 dir;
+
+    public float force = 300f;
 
     ScoreHandler scoreHandler;
-
 
     private void Awake()
     {
         scoreHandler = FindObjectOfType<ScoreHandler>();
+        rb = GetComponent<Rigidbody>();
     }
 
     public void OnCollisionEnter(Collision collision)
     {
-        if (collision.relativeVelocity.magnitude >= forceToDestroy)
+        if (collision.gameObject.name == "Player")
         {
-            Invoke("DestroyMe", 2.5f);
+            dir = collision.contacts[0].point - transform.position;
+            dir = -dir.normalized;
+            rb.AddForce(dir*force);
+            Invoke("DestroyMe", destroyCountdown);
         }
     }
 
 
-
     void DestroyMe()
     {
+        Instantiate(particles, transform.position, transform.rotation);
         Destroy(this.gameObject);
         scoreHandler.UpdateScore(destroyValue);
     }
